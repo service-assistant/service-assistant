7b71b11841e599c0b314b418f0e6f25d6ee08eb8
Mateusz Mańczak <46602256+mateuszmanczak04@users.noreply.github.com>
2026-04-10 12:31:11 +0200

    Setup dotenv in main.py

--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
-from app.routers import hello
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from fastapi import FastAPI  # noqa: E402
+from app.routers import hello  # noqa: E402
+
 
 app = FastAPI()
 
