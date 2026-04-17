d384f4d95f93c3c3b60440f3570f3b108b9749f7
Mateusz Mańczak <46602256+mateuszmanczak04@users.noreply.github.com>
2026-04-17 09:52:00 +0200

    Create POST /api/questions to orchestrate RAG

---
+++ server/app/services/embedding.py
@@ -0,0 +1,9 @@
+# TODO: fill these function and use correct types (not int and str)
+
+
+def embed_question(question: str) -> list[int]:
+    return [1, 2, 3]
+
+
+def get_close_chunks(embedded_vector: list[int]) -> list[str]:
+    return ["a", "b", "c"]
---
+++ server/app/services/llm.py
@@ -0,0 +1,6 @@
+# TODO: construct query to LLM and use correct Pydantic types
+
+
+def query(question: str, close_chunks: list[str]) -> str:
+    result = "some answer from LLM"
+    return result
