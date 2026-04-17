996f03b2401fb37388fa9ea5b6d62dfef59c388c
Mateusz Mańczak <46602256+mateuszmanczak04@users.noreply.github.com>
2026-04-17 09:19:28 +0200

    Create attachment_chunks db table

---
+++ server/alembic/versions/20260417_0855_5b668b89ccdc_create_attachment_chunks_table.py
@@ -0,0 +1,41 @@
+"""create_attachment_chunks_table
+
+Revision ID: 5b668b89ccdc
+Revises:
+Create Date: 2026-04-17 08:55:48.180562
+
+"""
+
+from typing import Sequence, Union
+
+import pgvector.sqlalchemy
+import sqlalchemy as sa
+import sqlmodel.sql.sqltypes
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+revision: str = "5b668b89ccdc"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("CREATE EXTENSION IF NOT EXISTS vector")
+    op.create_table(
+        "attachment_chunks",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("content", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
+        sa.Column("embedding", pgvector.sqlalchemy.Vector(1024), nullable=False),
+        sa.Column("document_name", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
+        sa.Column("page", sa.Integer(), nullable=False),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column(
+            "extra_metadata", postgresql.JSONB(astext_type=sa.Text()), nullable=True
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("attachment_chunks")
