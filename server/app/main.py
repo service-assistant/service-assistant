632d61f694db59f80cc5a6ae87b32ab543ff6772
Mateusz Mańczak <46602256+mateuszmanczak04@users.noreply.github.com>
2026-04-22 11:45:06 +0200

    Allow all cors in dev mode

--- server/app/main.py
+++ server/app/main.py
@@ -4,9 +4,20 @@ load_dotenv()
 
 from fastapi import FastAPI  # noqa: E402
 from app.routers import example, rag  # noqa: E402
+from fastapi.middleware.cors import CORSMiddleware  # noqa: E402
+import os  # noqa: E402
 
 
 app = FastAPI()
+
+if os.getenv("ENV") == "development":
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(example.router, prefix="/api")
 app.include_router(rag.router, prefix="/api")
