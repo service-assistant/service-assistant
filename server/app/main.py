3c11ba767503f8a25f15a30355ea3a1f1829e70b
Mateusz Mańczak <46602256+mateuszmanczak04@users.noreply.github.com>
2026-04-29 15:02:57 +0200

    Allow CORS from all origins

--- server/app/main.py
+++ server/app/main.py
@@ -9,14 +9,13 @@ app = FastAPI()
 
 settings = get_settings()
 
-if settings.env == "development":
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(example.router, prefix="/api")
 app.include_router(rag.router, prefix="/api")
